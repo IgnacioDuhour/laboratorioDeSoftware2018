a4a98d2db3a218579fb258971e1fe6f610883a4e
Ignacio Duhour <ignacioduhour@gmail.com>
2018-11-22 13:11:02 -0300

    proyecto sin comprimir

---
+++ app/src/main/java/com/laboratorio/entrega/razasypelejesdiazduhour/MainActivity.java
@@ -0,0 +1,37 @@
+package com.laboratorio.entrega.razasypelejesdiazduhour;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Activity act = this;
+
+        Button recButton = act.findViewById(R.id.button);
+        recButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(act, ReconocimientoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button playButton = act.findViewById(R.id.button2);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(act, ReconocimientoActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
