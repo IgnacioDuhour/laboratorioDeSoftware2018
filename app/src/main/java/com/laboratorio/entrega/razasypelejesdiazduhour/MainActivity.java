4286f1395788853bd89c973cb61eaa5ee82fffd0
Ignacio Duhour <ignacioduhour@gmail.com>
2018-11-24 11:54:25 -0300

    nuevos strings, activities configuracion y reconocimiento en blanco

--- app/src/main/java/com/laboratorio/entrega/razasypelejesdiazduhour/MainActivity.java
+++ app/src/main/java/com/laboratorio/entrega/razasypelejesdiazduhour/MainActivity.java
@@ -1,11 +1,14 @@
 package com.laboratorio.entrega.razasypelejesdiazduhour;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(act, ReconocimientoActivity.class);
                 startActivity(i);
+            }
+        });
+
+        ImageButton aboutButton = act.findViewById(R.id.imageButton2);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder about = new AlertDialog.Builder(act);
+                about.setMessage(R.string.about_message)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.about_dialog_btn, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                AlertDialog alert = about.create();
+                alert.setTitle(R.string.about_title);
+                alert.show();
             }
         });
     }
